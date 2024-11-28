948ff5463bc98ef4d34076c0f8968d7a27f44313
FajarRp11 <fajarrahyu1103@gmail.com>
2024-11-28 11:48:03 +0700

    Navbar, Home, task:content

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,11 +1,11 @@
 import { Link as LinkRoute } from "react-router-dom";
 
 const LinkButton = (props) => {
-  const { children, path } = props;
+  const { children, path, variant } = props;
   return (
     <LinkRoute
       to={path}
-      className="border-[2px] px-4 py-2 rounded-xl border-blue-600 mt-2 inline-block hover:bg-blue-600 hover:text-slate-200 transition-all duration-200"
+      className={`border-[2px] px-4 py-2 rounded-xl border-blue-600 mt-2 inline-block hover:bg-blue-600 hover:text-slate-200 transition-all duration-200 ${variant}`}
     >
       {children}
     </LinkRoute>
--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -4,37 +4,42 @@ import LinkButton from "../components/LinkButton";
 
 const Content = () => {
   return (
-    <div className="grid grid-cols-12 min-h-screen py-12" name="content">
-      <div className="col-span-12 px-2 py-4 md:col-span-6">
-        <div className="bg-slate-300 p-4 rounded-lg">
-          <h1 className="text-center font-bold text-2xl">BUDAYA</h1>
-          <img
-            src={Budaya}
-            alt="Tari Piring"
-            className="rounded-md my-2 aspect-video"
-          />
-          <p>
-            Kebudayaan merupakan penyelesaian manusia terhadap lingkungan
-            hidupnya, serta usaha untuk mempertahankan kelangsungan hidupnya
-            sesuai dengan keadaan (tradisi) yang terbaik.
-          </p>
-          <LinkButton path="/budaya">Pelajari Selengkapnya</LinkButton>
-        </div>
+    <div
+      className="grid grid-cols-12 min-h-screen py-16 px-4 gap-4"
+      name="content"
+    >
+      <div className="col-span-12 rounded-lg p-4 bg-slate-300 md:col-span-6 md:relative md:h-[550px]">
+        <h1 className="text-center font-bold text-2xl">BUDAYA</h1>
+        <img
+          src={Budaya}
+          alt="Tari Piring"
+          className="rounded-md my-2 aspect-video"
+          loading="lazy"
+        />
+        <p>
+          Kebudayaan merupakan penyelesaian manusia terhadap lingkungan
+          hidupnya, serta usaha untuk mempertahankan kelangsungan hidupnya
+          sesuai dengan keadaan (tradisi) yang terbaik.
+        </p>
+        <LinkButton path="/budaya" variant="md:absolute md:bottom-2">
+          Pelajari Selengkapnya
+        </LinkButton>
       </div>
-      <div className="col-span-12 px-2 py-4 md:col-span-6">
-        <div className="bg-slate-300 p-4 rounded-lg">
-          <h1 className="text-center font-bold text-2xl">NILAI LUHUR</h1>
-          <img
-            src={NilaiLuhur}
-            alt="Tari Piring"
-            className="rounded-md my-2 aspect-video"
-          />
-          <p>
-            Keyakinan seseorang yang mendorongnya untuk berperilaku dan bersikap
-            sesuai dengan standar atau pegangan tertentu.
-          </p>
-          <LinkButton path="/nilai-luhur">Pelajari Selengkapnya</LinkButton>
-        </div>
+      <div className="col-span-12 rounded-lg md:relative p-4 bg-slate-300 md:h-[550px] md:col-span-6">
+        <h1 className="text-center font-bold text-2xl">NILAI LUHUR</h1>
+        <img
+          src={NilaiLuhur}
+          alt="Nilai Luhur"
+          className="rounded-md my-2 aspect-video"
+          loading="lazy"
+        />
+        <p>
+          Keyakinan seseorang yang mendorongnya untuk berperilaku dan bersikap
+          sesuai dengan standar atau pegangan tertentu.
+        </p>
+        <LinkButton path="/nilai-luhur" variant="md:absolute md:bottom-2">
+          Pelajari Selengkapnya
+        </LinkButton>
       </div>
     </div>
   );
